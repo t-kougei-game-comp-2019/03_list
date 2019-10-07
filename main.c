e315507aa90fbf193dd5bd7fbf478187b6672208
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-08 05:26:14 +0900

    pushData関数の修正と関数群の使用

--- main.c
+++ main.c
@@ -8,17 +8,19 @@ typedef struct StackList{
 
 List* pushData(List *T,int data)
 {
-	List* tmp = (char *)malloc(sizeof(List));
+	List* tmp = (List*)malloc(sizeof(List));
    if(tmp == NULL) {
       printf("メモリが確保できません\n");
 	  return NULL;
    }
+   tmp->myData = data;
    tmp->Down = T;
    return tmp;
 }
 List* popData(List* T)
 {
 	List *tmp = T->Down;
+	printf("%d\n", T->myData);
 	free(T);
 	return tmp;
 }
@@ -26,7 +28,7 @@ void showList(List *T)
 {
 	while (T!=NULL)
 	{
-		printf("%d", T->myData);
+		printf("%d\n", T->myData);
 		T = T->Down;
 	}
 }
@@ -37,7 +39,12 @@ int main(int argc, char *argv[])
 	while (fgets(str, sizeof(str), stdin))
 	{
 		int tmp = strtol(str, NULL, 10);
-		
+		if (tmp == 0)
+			showList(TOP);
+		else if (tmp == -1)
+			TOP = popData(TOP);
+		else
+			TOP = pushData(TOP, tmp);
 	}
 
 	return 0;
