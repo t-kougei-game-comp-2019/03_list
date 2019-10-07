622c3fb996e4cf2f53afac3f1fd37127c580cb3e
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-08 04:29:13 +0900

    リスト構造体の操作関数を定義

--- main.c
+++ main.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
-struct StackList{
-	struct StackList* next;
-	int myData;
-};
+#include <stdlib.h>
 
+typedef struct StackList{
+	struct StackList* Down;
+	int myData;
+}List;
+
+List* pushData(List *T,int data)
+{
+	List* tmp = (char *)malloc(sizeof(List));
+   if(tmp == NULL) {
+      printf("メモリが確保できません\n");
+	  return NULL;
+   }
+   tmp->Down = T;
+   return tmp;
+}
+List* popData(List* T)
+{
+	List *tmp = T->Down;
+	free(T);
+	return tmp;
+}
+void showList(List *T)
+{
+	while (T!=NULL)
+	{
+		printf("%d", T->myData);
+		T = T->Down;
+	}
+}
 int main(int argc, char *argv[])
 {
     char str[256];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+	List *TOP=NULL;
+	while (fgets(str, sizeof(str), stdin))
+	{
+		int tmp = strtol(str, NULL, 10);
+		
+	}
 
-    }
-
-    return 0;
+	return 0;
 }
