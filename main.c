f6f2367b7ff0d7ca2c75086409629dcce68114b4
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-08 04:10:28 +0900

    構造体の定義

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+struct StackList{
+	struct StackList* next;
+	int myData;
+};
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-    char str[5];
+    char str[256];
     
     while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
+
     }
 
     return 0;
