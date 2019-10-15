3fa3553d4b56ad5f8fa593457680d626bfd8d53e
g1927046 <55736489+g1927046@users.noreply.github.com>
2019-10-15 16:47:09 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) 
+typedef struct StackList {
+	struct StackList* Down;
+	int myData;
+}List;
+
+List* pushData(List* T, int data)
 {
-    char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	List* tmp = (List*)malloc(sizeof(List));
+	if (tmp == NULL) {
+		printf("メモリが確保できません\n");
 
-    return 0;
+		return NULL;
+	}
+
+	tmp->myData = data;
+	tmp->Down = T;
+
+	return tmp;
+}
+
+List* popData(List* T)
+{
+	List* tmp = T->Down;
+	printf("%d\n", T->myData);
+
+	free(T);
+	return tmp;
+}
+
+void showList(List* T)
+{
+	while (T != NULL)
+	{
+		printf("%d", T->myData);
+		if (T->Down != NULL)
+			printf(",");
+		T = T->Down;
+	}
+	printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+	char str[256];
+	List* TOP = NULL;
+	while (fgets(str, sizeof(str), stdin))
+	{
+		int tmp = strtol(str, NULL, 10);
+		if (tmp == 0)
+			showList(TOP);
+		else if (tmp == -1)
+			TOP = popData(TOP);
+		else
+			TOP = pushData(TOP, tmp);
+	}
+
+	return 0;
 }
