424f9f55769c4551634776d5610e216c1823e9ba
yasu2520 <52170130+yasu2520@users.noreply.github.com>
2019-10-21 03:43:57 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,35 +4,30 @@
 int main(int argc, char *argv[])
 {
 	char str[5];
-	int array[100] = { 0 }, num = 0;
+	int array[100] = { 0 };
 	while (fgets(str, sizeof(str), stdin)) {
+
 		int input = atoi(str);
+
 		if (input == -1)
 		{
 			printf("%d\n",array[0]);
 			for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
 			{
 				if (i + 1 < sizeof(array) / sizeof(array[0]) && array[i] != 0)
-				{
 					array[i] = array[i + 1];
-				}
 				else
 					break;
 			}
 		}
 		else if(input == 0)
 		{
-			for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+			for (int i = 0; i < sizeof(array) / sizeof(array[0]) && array[i] != 0; i++)
 			{
-				if (array[i] != 0)
-				{
 					if(i == 0)
 						printf("%d", array[i]);
 					else
 						printf(",%d", array[i]);
-				}
-				else 
-					break;
 			}
 			printf("\n");
 		}
@@ -40,6 +35,7 @@ int main(int argc, char *argv[])
 		{
 			for (int i = 0; i <  sizeof(array) / sizeof(array[0]) - 1; i++)
 				array[sizeof(array) / sizeof(array[0]) - 1 - i] = array[sizeof(array) / sizeof(array[0]) - 2 - i];
+	
 			array[0] = input;
 		}
 	}
