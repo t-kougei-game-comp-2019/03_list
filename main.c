661e552423ee5e2e7b62df561226c6348a605b97
yasu2520 <52170130+yasu2520@users.noreply.github.com>
2019-10-21 04:07:17 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,40 +4,43 @@
 int main(int argc, char *argv[])
 {
 	char str[5];
-	int array[100] = { 0 }, debugPrint = 0;
+	int array[100] = { 0 }, avoidBug = 0;
 	while (fgets(str, sizeof(str), stdin)) {
 
 		int input = atoi(str);
 
-		if (input == -1)
+		if (avoidBug != input)
 		{
-			printf("%d\n",array[0]);
-			for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+			if (input == -1)
 			{
-				if (i + 1 < sizeof(array) / sizeof(array[0]) && array[i] != 0)
-					array[i] = array[i + 1];
-				else
-					break;
+				printf("%d\n", array[0]);
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+				{
+					if (i + 1 < sizeof(array) / sizeof(array[0]) && array[i] != 0)
+						array[i] = array[i + 1];
+					else
+						break;
+				}
 			}
-		}
-		else if(input == 0)
-		{
-			for (int i = 0; i < sizeof(array) / sizeof(array[0]) && array[i] != 0; i++)
+			else if (input == 0)
 			{
-					if(i == 0)
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]) && array[i] != 0; i++)
+				{
+					if (i == 0)
 						printf("%d", array[i]);
 					else
 						printf(",%d", array[i]);
+				}
+				printf("\n");
 			}
-			printf("(%d)\n",debugPrint);
-			debugPrint++;
-		}
-		else
-		{
-			for (int i = 0; i <  sizeof(array) / sizeof(array[0]) - 1; i++)
-				array[sizeof(array) / sizeof(array[0]) - 1 - i] = array[sizeof(array) / sizeof(array[0]) - 2 - i];
-	
-			array[0] = input;
+			else
+			{
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]) - 1; i++)
+					array[sizeof(array) / sizeof(array[0]) - 1 - i] = array[sizeof(array) / sizeof(array[0]) - 2 - i];
+
+				array[0] = input;
+			}
+			avoidBug = input;
 		}
 	}
 	return 0;
