d246c0f6e142bd08ea3a5b017e88b48d4cca8abb
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2020-01-26 18:29:59 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,51 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[]) 
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char* argv[])
 {
-    char str[5];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[5];
+	int array[100] = { 0 };
+	int n;
 
-    return 0;
+	while (fgets(str, sizeof(str), stdin)) {
+
+		int input = atoi(str);
+
+		if (input == -1)
+		{
+			printf("%d\n", array[0]);
+			for (int i = 0; i < sizeof(array) / sizeof(array[0]); i++)
+			{
+				if (i + 1 < sizeof(array) / sizeof(array[0]) && array[i] != 0)
+					array[i] = array[i + 1];
+				else
+					break;
+			}
+			n = 0;
+		}
+		else if (input == 0)
+		{
+			if (n == 0)
+			{
+
+				for (int i = 0; i < sizeof(array) / sizeof(array[0]) && array[i] != 0; i++)
+				{
+					if (i == 0)
+						printf("%d", array[i]);
+					else
+						printf(",%d", array[i]);
+				}
+				printf("\n");
+				n = 1;
+			}
+		}
+		else
+		{
+			for (int i = 0; i < sizeof(array) / sizeof(array[0]) - 1; i++)
+				array[sizeof(array) / sizeof(array[0]) - 1 - i] = array[sizeof(array) / sizeof(array[0]) - 2 - i];
+
+			array[0] = input;
+			n = 0;
+		}
+	}
+	return 0;
 }
